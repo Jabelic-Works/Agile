fa3e7d25f74076cd4e3e88a5e2b56b1081d95c6c
jabelic <hibiki990523@gmail.com>
2024-01-26 05:41:57 +0900

    feat: 🎸 第一章

---
+++ components/Logo.vue
@@ -0,0 +1,4 @@
+<!-- NOTE: Docusではcomponents/Logo.vueがロゴとして認識される:https://docus.dev/introduction/configuration -->
+<template>
+    <img src="agile-book_transparent.png" style="width:5vw; height:auto;">
+</template>
